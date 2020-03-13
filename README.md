8908c3f8540b9d873dcc1898d09ed83d3d9ba64b
davidwu <david6906817@gmail.com>
2020-03-14 00:41:15 +0800

    docker-compose

--- README.md
+++ README.md
@@ -4,19 +4,21 @@
 
 ## 建置 Docker 容器  
 
+因為開發環境的平台不一定會和佈署環境的平台相同，專案中的 Rust 程式會有跨平台的問題。  
+若在開發環境上建置一個和佈署環境相同平台(Ubuntu 18.04 x86_64)的容器，則可以借用該容器直接編譯出需要的 target 而不需要進行 cross compile。  
 開始編譯專案前須先確定編譯用的 Docker 容器正確運行中。  
 操作指令如下(詳細指令請參考 makefile)：  
 
 ```
-make build-img      # 製作 Docker image
-docker images       # 檢視已製作的 image
-make run-ctn        # 運行 container
-docker ps -a        # 檢視運行中的 container
-make login-ctn      # 登入 container
-
-make remove-ctn     # 終止並移除 container
-make remove-img     # 移除 image
+make builder-image          # 製作 builder image
+docker images               # 檢視已製作的 image
+make builder-container      # 運行 builder container
+docker ps -a                # 檢視運行中的 container
 ```
+
+## 安裝 NodeJS
+
+請使用 node v10.19.0
 
 ## 編譯專案  
 
--- build.bash
+++ build.bash
@@ -4,16 +4,25 @@ YELLOW="\x1B[1;33m"
 GREEN_FLASH="\x1B[1;5;32m"
 REST="\x1B[0m"
 
-rm -rf output && mkdir -p output
+function build-rust-repo() {
+    make build-rust repo=$1
+    mkdir -p pikit/app/$1
+    cp $1/target/release/$1 pikit/app/$1
+}
+
+function build-node-repo() {
+    make build-node repo=$1
+    mkdir -p pikit/app/$1
+    cp -r $1/dist/* pikit/app/$1
+}
+
+rm -rf pikit && mkdir pikit
+cp -r src/deploy/* pikit
 
 echo -e "\n${YELLOW}Building PIKIT service release...${REST}\n"
 
-RESTFUL_SERVER="pikit-restful-server"
-make build repo=$RESTFUL_SERVER
-cp $RESTFUL_SERVER/target/release/$RESTFUL_SERVER output/$RESTFUL_SERVER
-
-GRAPHQL_SERVER="pikit-graphql-server"
-make build repo=$GRAPHQL_SERVER
-cp $GRAPHQL_SERVER/target/release/$GRAPHQL_SERVER output/$GRAPHQL_SERVER
+build-rust-repo pikit-restful-server
+build-rust-repo pikit-graphql-server
+build-node-repo pikit-web
 
 echo -e "\n${GREEN_FLASH}Successfully builded.${REST}\n"
