68793f0fe54c72ce431b306292deb0e4197346c9
Wu Chao-Han <wuchao-han@WudeMacBook-Pro.local>
2020-04-29 23:32:04 +0800

    1.Added storage volume for restful-server. 2.Edit build.bash.

--- build.bash
+++ build.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 YELLOW="\x1B[1;33m"
 GREEN_FLASH="\x1B[1;5;32m"
@@ -6,18 +7,36 @@ REST="\x1B[0m"
 
 function build-rust-repo() {
     make build-rust repo=$1
-    mkdir -p pigskit/app/$1
+    mkdir pigskit/app/$1
     cp src/$1/target/release/$1 pigskit/app/$1
 }
 
 function build-node-repo() {
     make build-node repo=$1
-    mkdir -p pigskit/app/$1
+    mkdir pigskit/app/$1
     cp -r src/$1/dist/* pigskit/app/$1
 }
 
-rm -rf pigskit && mkdir pigskit
-cp -r src/deploy/* pigskit
+declare -a pigskit
+pigskit=(
+    app
+    docker
+    sql
+)
+
+if [ ! -d ./pigskit ]; then
+    mkdir pigskit
+fi
+
+for (( i = 0; i < ${#pigskit[*]}; i++ ))
+do
+    rm -rf pigskit/${pigskit[i]}
+    mkdir pigskit/${pigskit[i]}
+
+    if [ -d ./src/deploy/${pigskit[i]} ]; then
+        cp -r ./src/deploy/${pigskit[i]}/* ./pigskit/${pigskit[i]}
+    fi
+done
 
 echo -e "\n${YELLOW}Building Pigskit service release...${REST}\n"
 
